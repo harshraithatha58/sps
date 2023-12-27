1ba6d631cc0de1700f0a3f3521ca718f7cdc836e
Harsh Raithatha <harshraithatha123456@gmail.com>
2023-12-27 21:01:11 +0530

    test commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let userScore = 0;
+let computerScore = 0;
+
+const choises = document.querySelectorAll(".choise");
+
+choises.forEach((choise)=>{
+    // console.log(choise);
+    choise.addEventListener("click",()=>{
+
+    })
+})
